102bb522be4689097e037c24e0883e2b85ef1b4b
sparsh1408 <126798191+sparsh1408@users.noreply.github.com>
2024-08-07 23:09:43 +0530

    Add files via upload

---
+++ server/controllers/mainController.js
@@ -0,0 +1,21 @@
+
+exports.homepage = async (req, res) => {
+  const locals = {
+    title: "NodeJs Notes",
+    description: "Free NodeJS Notes App.",
+  }
+  res.render('index', {
+    locals,
+    layout: '../views/layouts/front-page'
+  });
+}
+
+
+
+exports.about = async (req, res) => {
+  const locals = {
+    title: "About - NodeJs Notes",
+    description: "Free NodeJS Notes App.",
+  }
+  res.render('about', locals);
+}
